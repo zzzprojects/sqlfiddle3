56c516885a27abc5311e63d0ba28c663fb2e2f5d
Jake Feasel <admin@sqlfiddle.com>
2016-12-28 16:19:19 -0800

    Successful demonstration of a connection to postgresql database

--- src/main/verticles/sqlfiddle.groovy
+++ src/main/verticles/sqlfiddle.groovy
@@ -2,9 +2,23 @@ import io.vertx.groovy.core.Vertx
 import io.vertx.groovy.ext.web.handler.StaticHandler
 import io.vertx.groovy.ext.web.Router
 
+import io.vertx.groovy.ext.jdbc.JDBCClient
+
 def server = vertx.createHttpServer()
 def router = Router.router(vertx)
+def jdbcConfig = [
+    url: "jdbc:postgresql://appdb:5432/sqlfiddle",
+    driver_class: "org.postgresql.Driver",
+    user: "postgres"
+]
+def client = JDBCClient.createShared(vertx, jdbcConfig, "SQLFiddle")
 
 router.route("/sqlfiddle/*").handler(StaticHandler.create())
+
+router.route("/dbTypes").handler({
+    client.getConnection({ res ->
+
+    })
+})
 
 server.requestHandler(router.&accept).listen(8080)
