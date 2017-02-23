9f2d4cafb6240843ec9d2202fb69f1b5e7d8da91
Jake Feasel <admin@sqlfiddle.com>
2017-02-23 11:43:15 -0800

    Provides first draft of working schema creation

--- appServer/src/main/verticles/SchemaDef.groovy
+++ appServer/src/main/verticles/SchemaDef.groovy
@@ -1,5 +1,7 @@
 import java.security.MessageDigest
 import io.vertx.core.json.JsonObject
+import io.vertx.groovy.ext.jdbc.JDBCClient
+import java.util.regex.Pattern
 
 class SchemaDef {
     private def vertx
@@ -18,73 +20,44 @@ class SchemaDef {
             content.statement_separator = ";"
         }
 
-        def schemaQuery
-        if (content.short_code) {
-            schemaQuery = [
-                sql: this.getSQL("WHERE s.short_code = ? AND s.md5 = ? AND s.db_type_id = ?"),
-                params: [
-                    content.short_code,
-                    md5hash,
-                    content.db_type_id
-                ]
-            ]
-        } else {
-            schemaQuery = [
-                sql: this.getSQL("WHERE s.md5 = ? AND s.db_type_id = ?"),
-                params: [
-                    md5hash,
-                    content.db_type_id
-                ]
-            ]
-        }
-        DatabaseClient.singleRead(this.vertx,
-            schemaQuery.sql,
-            schemaQuery.params,
-            { existing_schema ->
-                if (existing_schema != null) {
+        // see if we can find an existing schema that matches this md5 and db_type
+        DatabaseClient.singleRead(this.vertx, """
+            SELECT
+                d.id as db_type_id,
+                d.simple_name,
+                d.full_name,
+                d.context,
+                d.batch_separator,
+                s.id as schema_def_id,
+                s.short_code,
+                s.structure_json
+            FROM
+                db_types d
+                    LEFT OUTER JOIN schema_defs s ON
+                        d.id = s.db_type_id AND
+                        s.md5 = ?
+            WHERE
+                d.id = ?
+            """,
+            [ md5hash, content.db_type_id ],
+            { dbDetails ->
+                if (dbDetails == null) {
+                    throw new Exception("Unable to find database type details");
+                }
+
+                // schema_def already registered, return it
+                if (dbDetails.short_code != null) {
                     fn([
-                        _id: "${content.db_type_id}_${existing_schema.short_code}".toString(),
-                        short_code: existing_schema.short_code,
-                        schema_structure: existing_schema.structure_json != null ?
-                            new JsonObject(existing_schema.structure_json) : null
+                        _id: "${content.db_type_id}_${dbDetails.short_code}".toString(),
+                        short_code: dbDetails.short_code,
+                        schema_structure: dbDetails.structure_json != null ?
+                            new JsonObject(dbDetails.structure_json) : null
                     ])
                 } else {
-                    this.attemptToBuildSchema(content, md5hash, fn)
+                    this.registerSchema(content, md5hash, dbDetails, fn)
                 }
             }
         )
-    }
-
-    static private String getSQL(where) {
-        return """
-        SELECT
-            s.id,
-            s.md5,
-            s.db_type_id,
-            s.short_code,
-            to_char(s.last_used, 'YYYY-MM-DD HH24:MI:SS.MS') as last_used,
-            floor(EXTRACT(EPOCH FROM age(current_timestamp, last_used))/60) as minutes_since_last_used,
-            coalesce(s.ddl, '') as ddl,
-            s.statement_separator,
-            s.structure_json,
-            s.deprovision,
-            d.simple_name,
-            d.full_name,
-            d.context,
-            d.batch_separator,
-            coalesce(hosts_available.total, 0) as num_hosts_available
-        FROM
-            schema_defs s
-                INNER JOIN db_types d ON
-                    s.db_type_id = d.id
-                LEFT OUTER JOIN (
-                    SELECT h.db_type_id, count(*) as total
-                    FROM hosts h
-                    GROUP BY h.db_type_id
-                ) as hosts_available ON
-                    d.id = hosts_available.db_type_id
-        ${where}
-        """
     }
 
     private String getMD5(ddl, statement_separator) {
@@ -165,82 +138,224 @@ class SchemaDef {
         })
     }
 
-    private def attemptToBuildSchema(content, md5hash, fn) {
-
-//        def db_type = DBTypes.getDBType(this.vertx, content.db_type_id)
-/*
-        if (db_type.context == "host" && db_type.num_hosts == 0) {
-            throw new NoHostException("No host of this type available to create schema. Try using a different database version.")
-        }
-*/
-//        def structure = []
+    private def registerSchema(content, md5hash, dbDetails, fn) {
 
         this.getUniqueShortCode(content.db_type_id, md5hash, { short_code ->
-            this.create([
-                "db_type_id": content.db_type_id,
-                "short_code": short_code,
-                "md5": md5hash,
-                "ddl": content.ddl,
-                "statement_separator": content.statement_separator
-                //"structure": structure
-            ], { result ->
-                fn([
-                    _id: "${content.db_type_id}_${short_code}".toString(),
-                    short_code: short_code
-                    //schema_structure: existing_schema.structure_json != null ?
-                    //    new JsonObject(existing_schema.structure_json) : null
-                ])
-            })
-        })
-
-/*
-
-        // we only need to attempt to create a DB if the context for it is "host"
-        if (db_type.context == "host") {
-
-            // if there is an error thrown from here, it will be caught below;
-            // It is necessary to build the real db at this stage so that we can fail early if there
-            // is a problem (and get a handle on the real error involved in the creation)
-            def hostDetails = openidm.create("system/hosts/databases", null, [
-                "db_type_id": content.db_type_id,
-                "schema_name": "db_" + content.db_type_id + "_" + short_code,
-                "username": "user_" + content.db_type_id + "_" + short_code,
-                "pw": content.db_type_id + "_" + short_code,
-                "ddl": content.ddl ?: "",
-                "statement_separator": content.statement_separator
-            ])
-
-            Map schema_filters = [
-                "SQL Server": "dbo",
-                "MySQL": null,
-                "PostgreSQL": "public",
-                "Oracle": ("user_" + content.db_type_id + "_" + short_code).toUpperCase()
-            ]
-
-            if (schema_filters.containsKey(db_type.get('simple_name'))) {
-
-                Sql hostConnection = Sql.newInstance(hostDetails.get('jdbc_url'), hostDetails.get('username'), hostDetails.get('pw'), hostDetails.get('jdbc_class_name'))
-                hostConnection.withStatement { ((Statement) it).setQueryTimeout(10) }
-
-                structure = getSchemaStructure(hostConnection, db_type, schema_filters[db_type.get('simple_name')])
-
-                hostConnection.close()
-
+            // if the dbType context is "host", then we have to try to create the database in our
+            // backend environment before we try to save the schema definition
+            if (dbDetails.context == "host") {
+                this.buildRunningDatabase(
+                dbDetails,
+                short_code,
+                content.ddl,
+                content.statement_separator,
+                {
+                    this.create([
+                        "db_type_id": content.db_type_id,
+                        "short_code": short_code,
+                        "md5": md5hash,
+                        "ddl": content.ddl,
+                        "statement_separator": content.statement_separator,
+                        "structure": null
+                    ], { result ->
+                        fn([
+                            _id: "${content.db_type_id}_${short_code}".toString(),
+                            short_code: short_code
+                        ])
+                    })
+                })
+            } else { // context is not in our backend, so don't need to attempt to build it first
+                this.create([
+                    "db_type_id": content.db_type_id,
+                    "short_code": short_code,
+                    "md5": md5hash,
+                    "ddl": content.ddl,
+                    "statement_separator": content.statement_separator,
+                    "structure": null
+                ], { result ->
+                    fn([
+                        _id: "${content.db_type_id}_${short_code}".toString(),
+                        short_code: short_code
+                    ])
+                })
             }
-
-        }
-
-        // this schema_def will be linked to the above running db below as part of reconById
-        schema_def = openidm.create("system/fiddles/schema_defs", null, [
-            "db_type_id": content.db_type_id,
-            "short_code": short_code,
-            "md5": md5hash,
-            "ddl": content.ddl,
-            "statement_separator": content.statement_separator,
-            "structure": structure
-        ])
-
-        */
+        })
     }
+
+    private def findAvailableHost(db_type_id, fn) {
+        DatabaseClient.singleRead(this.vertx, """
+            SELECT
+                h.id,
+                h.db_type_id,
+                h.jdbc_url_template,
+                h.default_database,
+                h.admin_username,
+                h.admin_password,
+                d.setup_script_template,
+                d.drop_script_template,
+                d.batch_separator,
+                d.jdbc_class_name
+            FROM
+                Hosts h
+                    INNER JOIN db_types d ON
+                        h.db_type_id = d.id
+            WHERE
+                h.db_type_id = ? AND
+                not exists (
+                    SELECT
+                        1
+                    FROM
+                        Hosts h2
+                    WHERE
+                        h2.id != h.id AND
+                        h2.db_type_id = h.db_type_id AND
+                        coalesce((SELECT count(s.id) FROM schema_defs s WHERE s.current_host_id = h2.id), 0) <
+                        coalesce((SELECT count(s.id) FROM schema_defs s WHERE s.current_host_id = h.id), 0)
+                )
+        """, [db_type_id], fn)
+    }
+
+    private def getHostConnection(connectionDetails, connectionName, fn) {
+        JDBCClient
+            .createShared(this.vertx, connectionDetails, connectionName)
+            .getConnection({ hostConnectionHandler ->
+                if (hostConnectionHandler.succeeded()) {
+                    fn(hostConnectionHandler.result())
+                } else {
+                    throw "Unable to get connection: " +
+                        hostConnectionHandler.cause().getMessage()
+                }
+
+            })
+    }
+
+    private def executeSerially(connection, statements, fn) {
+        def executeHandler
+        executeHandler = { statementQueue ->
+            if (statementQueue.size() == 0) {
+                fn()
+            } else {
+                def statement = statementQueue.get(0)
+                statementQueue.remove(0)
+                println "EXECUTING STATEMENT:" + statement
+                connection.execute(statement, {
+                    if (it.succeeded()) {
+                        executeHandler(statementQueue)
+                    } else {
+                        println it.cause().getMessage()
+                        throw it
+                    }
+                })
+            }
+        }
+        executeHandler(statements)
+    }
+
+    private def executeScriptTemplate(hostConnection, script_template, batch_separator, databaseName, fn) {
+        String delimiter = (char) 7
+        String newline = (char) 10
+        String carrageReturn = (char) 13
+
+        // the scripts expect "databaseName" placeholders in the form of 2_abcde,
+        def script = script_template.replaceAll('#databaseName#', databaseName)
+
+        if (batch_separator && batch_separator.size()) {
+            script = script.replaceAll(Pattern.compile(newline + batch_separator + carrageReturn + "?(" + newline + '|$)', Pattern.CASE_INSENSITIVE), delimiter)
+        }
+        this.executeSerially(hostConnection, script.tokenize(delimiter), fn)
+    }
+
+    private def buildRunningDatabase(dbDetails, short_code, ddl, statement_separator, fn) {
+        // find a backend host capable of running our ddl...
+        findAvailableHost(dbDetails.db_type_id, { host ->
+
+            // get a connection to that host as an admin...
+            this.getHostConnection([
+                    url: host.jdbc_url_template.replace("#databaseName#", host.default_database),
+                    driver_class: host.jdbc_class_name,
+                    user: host.admin_username,
+                    password: host.admin_password
+                ],
+                "admin_" + dbDetails.db_type_id, // connection pool name
+                { adminHostConnection ->
+
+                    String databaseName = "${dbDetails.db_type_id}_${short_code}".toString()
+                    // use the admin host connection to setup a new, blank database in the host we have found...
+                    this.executeScriptTemplate(adminHostConnection,
+                        host.setup_script_template,
+                        host.batch_separator,
+                        databaseName,
+                        {
+
+                        // get a connection to the new, blank database as a non-admin user...
+                        this.getHostConnection([
+                                url: host.jdbc_url_template.replaceAll("#databaseName#", "db_" + databaseName),
+                                driver_class: host.jdbc_class_name,
+                                // assumes the setup script template builds the database with this pattern of credentials
+                                user: "user_" + databaseName,
+                                password: databaseName,
+                                initial_pool_size: 1,
+                                min_pool_size: 1,
+                                max_pool_size: 2,
+                                max_idle_time: 60
+                            ],
+                            "fiddle_" + databaseName, // connection pool name
+                            { hostConnection ->
+                                try {
+
+                                    // timeout queries in case someone is trying to run something crazy
+                                    // commented-out because it doesn't seem to work...?
+                                    //hostConnection.setQueryTimeout((int)10)
+
+                                    // consider refactoring below code to use executeScriptTemplate
+                                    String delimiter = (char) 7
+                                    String newline = (char) 10
+                                    String carrageReturn = (char) 13
+
+                                    // run the provided ddl to setup the database environment...
+                                    if (host.batch_separator && host.batch_separator.size()) {
+                                        ddl = ddl.replaceAll(Pattern.compile(newline + host.batch_separator + carrageReturn + "?(" + newline + '|$)', Pattern.CASE_INSENSITIVE), statement_separator)
+                                    }
+
+                                    // this monster regexp parses the query block by breaking it up into statements, each with three groups -
+                                    // 1) Positive lookbehind - this group checks that the preceding characters are either the start or a previous separator
+                                    // 2) The main statement body - this is the one we execute
+                                    // 3) The end of the statement, as indicated by a terminator at the end of the line or the end of the whole DDL
+                                    def statements = (Pattern.compile("(?<=(" + statement_separator + ")|^)([\\s\\S]*?)(?=(" + statement_separator + "\\s*\\n+)|(" + statement_separator + "\\s*\$)|\$)").matcher(ddl))
+                                        .findAll({
+                                            return (it[0].size() && ((Boolean) it[0] =~ /\S/) )
+                                        })
+                                        .collect({
+                                            return it[0]
+                                        })
+                                    this.executeSerially(hostConnection, statements, {
+                                        // statements must have executed successfully, close the various connections...
+                                        hostConnection.close({
+                                            adminHostConnection.close(fn)
+                                        })
+                                    });
+                                } catch (e) {
+                                    // something went wrong - probably bad ddl
+                                    // close the non-admin host connection...
+                                    hostConnection.close({
+                                        // remove the database from the host...
+                                        this.executeScriptTemplate(adminHostConnection,
+                                            host.drop_script_template,
+                                            host.batch_separator,
+                                            databaseName,
+                                            {
+                                                adminHostConnection.close(fn)
+                                                throw new Exception(e.getMessage())
+                                            }
+                                        )
+                                    })
+                                } // end try-catch
+                            }) // end host connection
+                        }) // end setup of host database
+                    }) // end admin connection
+                }) // end find available host
+
+    } // end buildRunningDatabase
+
 
 }
